5644e36c7650ddc46b27c8ad2530fc19e4980946
onerciller <onerciller@gmail.com>
2016-02-29 22:53:56 +0200

    updated file

--- components/Editor.js
+++ components/Editor.js
@@ -7,13 +7,16 @@ import 'brace/mode/html';
 import 'brace/theme/monokai';
 
 
+var initialSource =  '<html>\n<head>\n\n</head>\n\n <body>\n\n<h1>SAMPLE 1</h1>\n<h2>SAMPLE 2</h2>\n<h3>SAMPLE 3</h3>\n\n<p>Neque porro quisquam est qui dolorem ipsum quia dolor \n <br> sit amet, consectetur, adipisci velit.</p></body>\n\n</html>';
 
 export default class Editor extends Component{
 
+
   constructor(props){
+    var localS = localStorage.getItem("token");
     super(props);
     this.state = {
-      value:localStorage.getItem("token")
+      value: localS == "" ? initialSource : localS
     }
 
   }
@@ -27,16 +30,13 @@ localStorage.setItem("token", value );
     const {value} = this.state;
       return (
         <div>
-        <AceEditor mode="html" theme="monokai" fontSize={16} height="800"  width="670" value ={value != "" ? value : localStorage.getItem("token")} onChange={this.handleChange.bind(this)} name="editor" editorProps={{$blockScrolling: Infinity}} />
+        <AceEditor mode="html" theme="monokai" fontSize={16} height="800"  width="670" value ={value } onChange={this.handleChange.bind(this)} name="editor" editorProps={{$blockScrolling: Infinity}} />
         <div id="output">
-          <Output  output= {value != "" ? value : localStorage.getItem("token") } />
+          <Output  output= {value} />
         </div>
       </div>
 
       );
   }
-
-  
-
 
 }
